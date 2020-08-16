2975ac961f840daba3514dbf27f0cd8f13e05df5
Carlos Martinez <edu6jas@gmail.com>
2020-08-16 01:34:51 -0400

    testing offline 3

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,6 +1,9 @@
 const CACHE_NAME = 'weatherapp-cache';
 // const urlsToCache = ['index.html', 'offline.html', '/country'];
 const urlsToCache = [
+    'index.html',
+    'offline.html',
+    '/country',
     'https://pwa-prueba.netlify.app/index.html',
     'https://pwa-prueba.netlify.app/offline.html',
     'https://pwa-prueba.netlify.app/country'
@@ -24,11 +27,16 @@ self.addEventListener('install', async (event) => {
 
 // 2) listen for requests
 self.addEventListener('fetch', (event) => {
+    console.log("hace fetch");
     event.respondWith(
         caches.match(event.request)
             .then( () => {
+                console.log("encontró la página y tratará de traerla");
                 return fetch(event.request)
-                    .catch( () => caches.match('offline.html')) // in case is offline
+                    .catch( () => {
+                        console.log("no la pudo traer, porque el server está offline");
+                        return caches.match('offline.html');// in case is offline
+                    }) 
             })
     )
 });
